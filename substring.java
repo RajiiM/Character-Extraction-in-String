d50a1a9dd2cfcbbce6898bf228187dae510e7f7c
RajiiM <mrajalakshmiofficial@gmail.com>
2021-12-05 01:16:15 +0530

    FirstCommit

---
+++ substring.java
@@ -0,0 +1,15 @@
+public class Practice {
+
+	public static void main(String[] args) {
+      String s1 = "Payilagam Institute Chennai";
+      String s2 = "Raja Raja Chozhan";
+
+      System.out.println(s1.substring(10)); 
+      System.out.println(s2.substring(5,9)); 
+      //Begin index=Start, End Index-1=End
+	}    
+}
+
+//OUTPUT
+Institute Chennai
+Raja
