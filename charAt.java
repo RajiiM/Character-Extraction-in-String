15d5fd83011c9cb29a5c133255802916df1d71c3
RajiiM <mrajalakshmiofficial@gmail.com>
2021-12-05 01:14:55 +0530

    FirstCommit

---
+++ charAt.java
@@ -0,0 +1,11 @@
+public class Practice {
+
+	public static void main(String[] args) {
+      String s1 = "Payilagam Institute Chennai";
+
+      System.out.println(s1.charAt(3));     
+	}    
+}
+
+//OUTPUT
+i
